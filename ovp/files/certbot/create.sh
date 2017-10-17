e11885c55146519d0b484152d7a63f0b0a4518a9
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-10-17 16:55:32 +0000

    Add pv admin domain to certbot

--- ovp/files/certbot/create.sh
+++ ovp/files/certbot/create.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo certbot certonly --nginx -d api.beta.atados.com.br --non-interactive --agree-tos --email webmaster@atados.com.br
+sudo certbot certonly --nginx -d pv.admin.beta.atados.com.br --non-interactive --agree-tos --email webmaster@atados.com.br
